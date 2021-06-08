b75eba76e3c2df28345d793bb1941ba20103c768
Stan Ferguson <stan.ferguson@gmail.com>
2021-06-08 09:05:35 -0500

    added icon codes

--- server.R
+++ server.R
@@ -15,11 +15,31 @@ shinyServer(function(input, output) {
         cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
     }, ignoreNULL = FALSE)
     
+    victimIconsiter <- (
+    for (x in seq_along(reactiveMain()$victim)) {
+        victimIconsiter <- reactiveMain()$URL.of.image.of.victim
+    })
+         
+    
+    victimIcons <- icons(
+        iconUrl = victimIconsiter,
+                         #$URL.of.image.of.victim,
+        iconWidth = 28, iconHeight = 28
+    )
+    
     output$map <- renderLeaflet({
-        leaflet(victims_geo) %>%
+        validate(
+            need(input$race, 'Check at least one race'),
+            need(input$gender, 'Check at least one gender'),
+            need(input$armed, 'Check at least one armed status'))
+        
+        
+        leaflet(reactiveMain()) %>%
             setView(lng = -96,lat =  37.8, zoom = 4)%>%
             addProviderTiles("CartoDB.Positron", 
-                             options = providerTileOptions(noWrap = TRUE))
+                             options = providerTileOptions(noWrap = TRUE))%>%
+            addMarkers(lng = ~Longitude, lat = ~Latitude, icon = victimIcons,
+                       popup = ~as.character(victim), label = ~as.character(victim))
     })
     
     reactiveMain <- reactive({
